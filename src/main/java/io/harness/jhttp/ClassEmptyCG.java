5eaaa5550081038a30e6a46ff84e6b7150c06c5c
Aishwarya Lad <67022814+Aishwarya-Lad@users.noreply.github.com>
2023-11-28 13:33:34 -0800

    just ti (#129)

---
+++ src/main/java/io/harness/jhttp/ClassEmptyCG.java
@@ -0,0 +1,13 @@
+package io.harness.jhttp;
+
+/**
+ * Introduced this class for verification of TI for empty CG
+ *
+ */
+
+
+public class ClassEmptyCG{
+    static {
+        System.out.println("Loading ClassEmptyCG to test TI for empty CG");
+    }
+}
